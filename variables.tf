f456da6bd2ba41d5be5c6cc2eddc0a57131631bc
isuridayara <dayaraindrarathna@gmail.com>
2025-10-17 19:48:58 +0300

    Variable Changed

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "student_name" {
   description = "Unique student identifier"
   type        = string
-<<<<<<< HEAD
+
   default     = "isuridayara" # Change this to your own unique name
-=======
+
   default     = "manjithatest8" # Change this to your own unique name
->>>>>>> 59cf8b0c1d4541162f46ca9f9ff1ca94cd9dbdc4
+
 }
 
 variable "aws_region" {
